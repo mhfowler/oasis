2f9361fb415d58ec1d8bc182de425aa6e7d6b32e
Max Fowler <maxhfowler@gmail.com>
2016-05-14 20:49:22 -0400

    "its oasis"

--- pi_utilities/announce_ip.py
+++ pi_utilities/announce_ip.py
@@ -41,7 +41,7 @@ def announce_ip():
             print routes
             print '++ endroutes'
             if ip_address:
-                slack_notify_message('@channel: its pi: {} | {}'.format(str(ip_address), str(current_connection)))
+                slack_notify_message('@channel: its oasis: {} | {}'.format(str(ip_address), str(current_connection)))
                 break
         except Exception as e:
             print ':/ error: {}'.format(str(e.message))
