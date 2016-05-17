32ac6f66a4af64781d0da57a74f397b8c9c11b63
Max Fowler <maxhfowler@gmail.com>
2016-05-17 01:02:36 -0400

    create new users from dms

--- js/create_new_users.js
+++ js/create_new_users.js
@@ -1,40 +1,144 @@
+var Promise = require('promise');
 var twitter_helper = require ('./twitter_helper.js');
 var _log = require('./log_helper.js')._log;
 var child_process = require('child_process');
 var util = require('util');
 var settings = require('./settings.js');
 var path = require("path");
+var fs = require('fs');
+
+
+var dm_id_file = path.join(settings.base_dir, '../data/dm_id.txt');
 
 
 var exec_cmd = function(cmd, success_msg) {
-    _log(util.format('cmd: %s', cmd));
-    child_process.exec(cmd, function(error, stdout, stderr) {
-        _log(util.format('stdout: %s', stdout));
-        _log(util.format('stderr: %s', stderr));
-        _log(util.format('error: %j', error));
-        if (error == null && success_msg != null) {
-            _log(success_msg);
-        }
+    return new Promise(function (fulfill, reject) {
+        _log(util.format('cmd: %s', cmd));
+        child_process.exec(cmd, function (error, stdout, stderr) {
+            //_log(util.format('stdout: %s', stdout));
+            //_log(util.format('stderr: %s', stderr));
+            _log(util.format('error: %j', error));
+            if (error) {
+                reject(error);
+            }
+            else if (error == null && success_msg != null) {
+                _log(success_msg);
+                fulfill(error);
+            }
+            else {
+                fulfill(error);
+            }
+        });
     });
-}
+};
 
 var create_user = function(username, password) {
     username = username.replace(/\W/g, '');
     password = password.replace(/\W/g, '');
     var create_user_script = path.join(settings.base_dir, 'bash/create_new_user.sh');
     var create_user_cmd = util.format('%s %s %s', create_user_script, username, password);
-    exec_cmd(create_user_cmd, util.format('++ @channel: successfully created user: %s', username));
-}
+    var create_user_cmd = 'ls';
+    return exec_cmd(create_user_cmd, util.format('++ @channel: successfully created user: %s', username));
+};
+
+
+var process_dm = function(dm) {
+    var success = false;
+    console.log(util.format('screen_name: %s', dm['sender_screen_name']));
+    console.log(util.format('message: %s', dm['text']));
+    console.log(util.format('id: %s', dm['id']));
+    // write id to a file
+    var myRegexp = /(\S+)\s*\/\s*(\S+)/g;
+    var match = myRegexp.exec(dm['text']);
+    if (match) {
+        var username = match[1];
+        var password = match[2];
+        console.log(util.format('++ create user: %s %s', username, password));
+        var user_promise = create_user(username, password);
+        // now tweet back at the user that their user was created
+        user_promise.done(function(res) {
+            twitter_helper.post_dm('OK', dm['sender_screen_name']);
+            // get most recent tweet of correct format
+            var tweets_promise = twitter_helper.get_timeline();
+            tweets_promise.done(function(response) {
+                var tweets = JSON.parse(response['body']);
+                console.log(util.format('tweet: %j', Object.keys(tweets[0])));
+                var host = '';
+                var port = '';
+                var index = 0;
+                while (host == '') {
+                    var tweet = tweets[index];
+                    var myRegexp = /ssh \S+@(\S+) -p(\S+)/g;
+                    var match = myRegexp.exec(tweet['text']);
+                    if (match) {
+                        host = match[1];
+                        port = match[2];
+                    }
+                    index += 1;
+                }
+                twitter_helper.post_tweet(util.format('@%s ssh %s@%s -p%s', dm['sender_screen_name'], username, host, port));
+                success = true;
+            });
+        }, function(err) {
+            twitter_helper.post_dm('500: invalid username', dm['sender_screen_name']);
+        });
+        // ls /home
+    }
+    else {
+        twitter_helper.post_dm('500: invalid username', dm['sender_screen_name']);
+    }
+    if (!success) {
+        _log(util.format('@channel: failed to process dm for @%s', dm['sender_screen_name']));
+    }
+};
+
+
+var save_dm_id = function(dm_id) {
+    fs.writeFile(dm_id_file, dm_id, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log(util.format('++ saved dm_id: %s', dm_id));
+    });
+};
+
+var read_dm_id = function() {
+    return new Promise(function (fulfill, reject) {
+        if (fs.existsSync(dm_id_file)) {
+            fs.readFile(dm_id_file, 'utf8', function (err, data) {
+                if (err) {
+                    reject(err);
+                }
+                fulfill(data);
+            });
+        }
+        else {
+            fulfill(0);
+        }
+    });
+};
 
 
 var create_new_users = function() {
     _log('++ looking for new users to create');
-    var new_users = [];
-    for (i = 0; i < new_users.length; i++) {
-        var user_info = new_users[i];
-        create_user(user_info['username'], user_info['password']);
-    }
-}
+    // read id from a file
+    var last_dm_id_promise = read_dm_id();
+    last_dm_id_promise.done(function(dm_id) {
+        console.log(util.format('last_dm_id: %s', dm_id));
+        var dms_promise = twitter_helper.get_dms(dm_id);
+        console.log('dms_promise: %j', dms_promise);
+        dms_promise.done(function(response) {
+            var dms = JSON.parse(response['body']);
+            for (i = 0; i < dms.length; i++) {
+                var dm = dms[i];
+                save_dm_id(dm['id']);
+                process_dm(dm);
+            }
+        });
+    });
+};
+
+
 
 if (require.main === module) {
     create_new_users();
--- js/twitter_helper.js
+++ js/twitter_helper.js
@@ -1,4 +1,4 @@
-
+var Promise = require('promise');
 var secrets = require('../ansible-pi/secret_files/secret.json');
 var util = require('util');
 var Twitter = require('twitter');
@@ -15,24 +15,96 @@ var access_token_secret = secrets['TWITTER_ACCESS_TOKEN_SECRET'];
 //console.log('access_token_secret: %s', access_token_secret);
 
 var client = new Twitter({
-  consumer_key: consumer_key,
-  consumer_secret: consumer_secret,
-  access_token_key: access_token_key,
-  access_token_secret: access_token_secret
+    consumer_key: consumer_key,
+    consumer_secret: consumer_secret,
+    access_token_key: access_token_key,
+    access_token_secret: access_token_secret
 });
 
 
 exports.post_tweet = function(msg) {
-  client.post('statuses/update', {status: msg},  function(error, tweet, response){
-    if(error) {
-      console.log(util.format('error: %j', error));
+    client.post('statuses/update', {status: msg},  function(error, tweet, response){
+        if(error) {
+            console.log(util.format('error: %j', error));
+        };
+    });
+}
+
+
+exports.get_dms = function(since_id) {
+    var params = {};
+    if (since_id != null) {
+        params = {'since_id': since_id}
+    }
+    return new Promise(function (fulfill, reject) {
+        client.get('direct_messages', params,  function(error, tweet, response){
+            if(error) {
+                console.log(util.format('error: %j', error));
+                reject(error);
+            }
+            else {
+                fulfill(response);
+            }
+        });
+    });
+}
+
+
+exports.get_timeline = function() {
+    var params = {
+        exclude_replies: true,
+        screen_name: 'ss022001',
+        count: 100
     };
-  });
+    return new Promise(function (fulfill, reject) {
+        client.get('statuses/user_timeline', params,  function(error, tweet, response){
+            if(error) {
+                console.log(util.format('error: %j', error));
+                reject(error);
+            }
+            else {
+                fulfill(response);
+            }
+        });
+    });
+}
+
+
+exports.post_dm = function(msg, send_to) {
+    client.post('/direct_messages/new', {screen_name: send_to, text: msg},  function(error, tweet, response){
+        if(error) {
+            console.log(util.format('error: %j', error));
+        };
+    });
+}
+
+
+function readJSON(filename){
+    return new Promise(function (fulfill, reject){
+        readFile(filename, 'utf8').done(function (res){
+            try {
+                fulfill(JSON.parse(res));
+            } catch (ex) {
+                reject(ex);
+            }
+        }, reject);
+    });
 }
 
 
 if (require.main === module) {
-    post_tweet('++ hello');
+    var dms_promise = exports.get_dms(null);
+    console.log('dms_promise: %j', dms_promise);
+    dms_promise.done(function(response) {
+        var dms = JSON.parse(response['body']);
+        console.log(util.format('dm: %j', Object.keys(dms[0])));
+        for (i = 0; i < dms.length; i++) {
+            var dm = dms[i];
+            console.log(util.format('screen_name: %s', dm['sender_screen_name']));
+            console.log(util.format('message: %s', dm['text']));
+            console.log(util.format('id: %s', dm['id']));
+        }
+    });
 }
 
 
