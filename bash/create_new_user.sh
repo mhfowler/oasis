729d614b4025000472b37ea660992b4a04b455c1
Max Fowler <maxhfowler@gmail.com>
2016-05-17 08:42:03 -0400

    home folders

--- bash/create_new_user.sh
+++ bash/create_new_user.sh
@@ -7,3 +7,8 @@ PASSWORD=$2
 
 /usr/sbin/useradd -G oasis -m $USERNAME
 /bin/echo "$USERNAME:$PASSWORD" | /usr/sbin/chpasswd
+
+# give correct permission
+/bin/chgrp oasis /home/$USERNAME
+/bin/chown 640 /home/$USERNAME
+ln -s  /greetingroom /home/$USERNAME/greetingroom
