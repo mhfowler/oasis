43ca0abdc1e173fcbb13639ac0896b32bd52fa68
Max Fowler <maxhfowler@gmail.com>
2016-10-23 02:29:01 -0400

    profile

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 #ansible -i ansible-pi/hosts pis -m ping --ask-pass --sudo
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo --tags code
-#ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo
-ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo --tags code
+ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo
+#ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo --tags code
