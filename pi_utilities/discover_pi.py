e426c6570fe081e3fc2625b2942bea9d8753e2f1
Max Fowler <maxhfowler@gmail.com>
2016-11-01 21:40:50 -0400

    updates to guide

--- pi_utilities/discover_pi.py
+++ pi_utilities/discover_pi.py
@@ -1,23 +1,24 @@
 import subprocess
 from hello_settings import SECRETS_DICT, PROJECT_PATH
-import paramiko
+# import paramiko
 import os
 
 
 def discover_pi():
     # ips = subprocess.check_output("/usr/local/bin/nmap 192.168.0.1/24 -n -sP | grep report | awk '{print $5}'", shell=True)
     # ips = subprocess.check_output("/usr/local/bin/nmap 172.20.10.2/24 -n -sP | grep report | awk '{print $5}'", shell=True)
-    ips = subprocess.check_output("/usr/local/bin/nmap 192.168.0.8/24 -n -sP | grep report | awk '{print $5}'", shell=True)
+    # ips = subprocess.check_output("/usr/local/bin/nmap 192.168.0.8/24 -n -sP | grep report | awk '{print $5}'", shell=True)
+    ips = subprocess.check_output("/usr/local/bin/nmap 172.16.0.254/24 -n -sP | grep report | awk '{print $5}'", shell=True)
     for ip_address in ips.split('\n'):
         print ip_address
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(ip_address, username=SECRETS_DICT['PI_USERNAME'], password=SECRETS_DICT['PI_PASSWORD'])
-            print '++ found pi: {}'.format(ip_address)
-        except Exception as e:
-            print str(e.message)
-            continue
+        # try:
+        #     ssh = paramiko.SSHClient()
+        #     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #     ssh.connect(ip_address, username=SECRETS_DICT['PI_USERNAME'], password=SECRETS_DICT['PI_PASSWORD'])
+        #     print '++ found pi: {}'.format(ip_address)
+        # except Exception as e:
+        #     print str(e.message)
+        #     continue
 
 
 if __name__ == '__main__':
